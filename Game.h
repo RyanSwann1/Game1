063788df889ffbbf9c73b00df06c8bdc095732fb
Ryan Swann <RyanSwann1992@gmail.com>
2016-04-12 11:49:55 +0100

    Update Game.h

--- Game.h
+++ Game.h
@@ -21,13 +21,15 @@ public:
 
 
 	bool isRunning() const { return m_running; }
-
+	SDL_Surface* getWindowSurface() const { return m_windowSurface; }
+	SDL_Renderer* getRenderer() const { return m_renderer; }
 
 
 private:
 
 	static Game* m_instance;
 	SDL_Window* m_window;
+	SDL_Surface* m_windowSurface;
 	SDL_Renderer* m_renderer;
 	TextureManager m_textureManager;
 
