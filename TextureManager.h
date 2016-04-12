9e5a06112aebb1d4e1ff49f1fb68a2a37814fbed
Ryan Swann <RyanSwann1992@gmail.com>
2016-04-12 11:49:04 +0100

    Update TextureManager.h

--- TextureManager.h
+++ TextureManager.h
@@ -2,6 +2,7 @@
 #define TEXTURE_MANAGER_H
 
 #include <SDL.h>
+#include <map>
 
 class TextureManager
 {
@@ -9,11 +10,12 @@ public:
 	TextureManager();
 	~TextureManager();
 
-	void load();
+	void load(std::string ID, std::string path);
 	void draw(int x, int y, int width, int height, SDL_Renderer *renderer);
 
 
 private:
+	std::map<std::string, SDL_Texture*> m_textures;
 };
 #endif // !TEXTURE_MANAGER_H
 
